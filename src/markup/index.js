4800b4c1a3dd0100b8c0ec9419da4a5ec70fbc9c
Mike <50363065+MikeGankin@users.noreply.github.com>
2026-05-27 18:06:13 +0300

    Initial commit

---
+++ src/markup/index.js
@@ -0,0 +1,4 @@
+import h0 from './welcome.html?raw';
+import h1 from './text.html?raw';
+
+export default [{ key: "welcome", html: h0 }, { key: "text", html: h1 }];
---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+import init0 from './welcome.js';
+import init1 from './text.js';
+
+export default new Map([["welcome", init0], ["text", init1]]);
